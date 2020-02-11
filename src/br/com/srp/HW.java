e18f40a2ddb0ac17b1a691907241748c07032825
Grego S/A <stefanospavlidis@gmail.com>
2020-02-11 11:38:17 -0300

    Primeiro commit

---
+++ src/br/com/srp/HW.java
@@ -0,0 +1,17 @@
+package br.com.srp;
+
+import java.util.Collections;
+
+public class HW {
+	
+	public static void main(String[] args) {
+		String teste="";
+		
+		
+		teste = String.join("#", "hello","world","baby");
+		
+		System.out.println(teste);
+		
+	}
+
+}
---
+++ src/br/com/srp/builder/BuilderTeste.java
@@ -0,0 +1,85 @@
+package br.com.srp.builder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BuilderTeste {
+	
+	public static void main(String[] args) {
+		
+		
+		String hello = "hello";
+		
+		System.out.println("<p>" + hello + "<p>");
+		
+		String words[] = {"hello", "world","baby"};
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("<ul>\n");
+		
+		for(String word:words) {
+			sb.append(String.format("  <li>%s</li>\n", word));
+		}
+		
+		sb.append("</ul>");
+		
+		System.out.println(sb);
+		
+	}
+	
+	class HtmlElement {
+		public String name, text;
+		public ArrayList<HtmlElement> elements = new ArrayList<>();
+		private final int indentSize = 2;
+		private final String newLine = System.lineSeparator();
+		
+		
+		public HtmlElement() {
+			super();
+		}
+
+
+		public HtmlElement(String name, String text) {
+			super();
+			this.name = name;
+			this.text = text;
+		}
+		
+		private String toStringImpl(int indent) {
+			
+			StringBuilder sb = new StringBuilder();
+			String i = String.join("", Collections.nCopies(indent * indentSize, " "));
+			sb.append(String.format("%s<%s>%s", i, name, newLine));
+			
+			if(text != null && !text.isEmpty()) {
+				sb.append(String.join("",Collections.nCopies(indentSize*(indent+1), " ")))
+						.append("text")
+						.append(newLine);
+			}
+			
+			for(HtmlElement e:elements) {
+				sb.append(e.toStringImpl(indent + 1));
+			}
+			
+			sb.append(String.format("%s<%s>%s", i, name, newLine));
+			
+			return sb.toString();
+			
+		}
+		
+		@Override
+		public String toString() {
+			return toStringImpl(0);
+			
+		}
+		
+		
+	} 
+	
+	
+	class HtmlBuilder{
+		
+	}
+
+}
